118c89a4adafb9b64587e957d865bc9d36996fb5
117XinyuLi <109027904+117XinyuLi@users.noreply.github.com>
2023-12-10 20:16:13 +0800

    Add files via upload

---
+++ 五级流水CPU/CPU.srcs/sources_1/imports/lab_4/hazard.v
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+
+module hazard(
+	input wire[4:0] rsE, rtE, writeregM, writeregW,
+	input wire regwriteM, regwriteW,
+	output wire[1:0] forwardAE, forwardBE,
+
+	input wire[4:0] rsD, rtD,
+	input wire memtoregE,
+	output wire stallF, stallD, flushE,
+
+	output forwardAD, forwardBD,
+	input wire branchD, regWriteE, 
+	input wire [4:0]writeregE
+);
+
+	// 前推：需要的寄存器号和写回的寄存器号对比，( rsE != 5'b0 ) 0号寄存器不用，
+	// ( rsE == writeregM ) 读取的寄存器号和写回的寄存器号相同，regwriteM 写回使能
+	assign forwardAE = (( rsE != 5'b0 ) & ( rsE == writeregM ) & regwriteM) ? 2'b10 :
+						(( rsE != 5'b0 ) & ( rsE == writeregW ) & regwriteW) ? 2'b01 : 2'b00;
+
+	assign forwardBE = (( rtE != 5'b0 ) & ( rtE == writeregM ) & regwriteM) ? 2'b10 :
+						(( rtE != 5'b0 ) & ( rtE == writeregW ) & regwriteW) ? 2'b01 : 2'b00;
+
+	assign forwardAD = (rsD != 5'b0) & (rsD == writeregM) & regwriteM;
+	assign forwardBD = (rtD != 5'b0) & (rtD == writeregM) & regwriteM;
+
+	// 暂停
+	wire lwstall;
+	wire branch_stall;
+	assign branch_stall = branchD & regWriteE & ((writeregE == rsD) | (writeregE == rtD)) |
+							branchD & regwriteM & ((writeregM == rsD) & (writeregM == rtD)); 
+	assign lwstall = ((rsD == rtE) | (rtD == rtE)) & memtoregE;
+	assign stallF = lwstall | branch_stall;
+	assign stallD = lwstall | branch_stall;
+	assign flushE = lwstall | branch_stall;
+
+
+endmodule
---
+++ 五级流水CPU/CPU.srcs/sources_1/new/sim_CPU.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+
+
+module sim_CPU();
+    reg CLK;
+    reg Rst;
+    wire R_W;
+    wire [31:0] pc, instr, ALU_result;
+    wire zero,memtoreg,regwrite,branch,alusrc,regdst,jump;
+    wire [2:0] alucontrol;
+
+
+    initial 
+    begin
+        CLK = 1;
+        forever #50 CLK = ~CLK;
+        #2000 $stop(0);
+    end
+    initial
+    begin
+        Rst = 0;
+        #0 Rst=1;
+        #10 Rst=0;
+    end
+    mips CPU(
+        .clk(CLK),
+        .rst(Rst),
+        .R_W(R_W),
+        .pc(pc),
+        .instr(instr),
+        .ALU_result(ALU_result),
+        .zeroM(zero),
+        .memtoreg(memtoreg),
+        .regwrite(regwrite),
+        .branch(branch),
+        .alusrc(alusrc),
+        .regdst(regdst),
+        .jump(jump),
+        .alucontrol(alucontrol)
+    );
+endmodule
